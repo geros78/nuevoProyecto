49079fa65d6de0c2d43ccd151ea11ee9943e2920
geros78 <alejo.o.o1000@gmail.com>
2022-10-04 08:46:59 -0500

    Funcion peso a dolar

---
+++ app.js
@@ -0,0 +1,14 @@
+function imc(peso, altura){
+    return Math.pow(peso/(altura,2))
+}
+
+function suma(num1, num2){
+    return num1 + num2;
+}
+
+function cambio(pesos){
+    return pesos/4500;
+}
+
+console.log(suma(10,9));
+console.log(cambio(120000));
